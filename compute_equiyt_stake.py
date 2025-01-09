0921127a8deeff1e8f429c69604484b43123b542
AndreFelix74 <felixsouza74@gmail.com>
2025-01-09 19:44:21 -0300

    fiz allocation

--- compute_equiyt_stake.py
+++ compute_equiyt_stake.py
@@ -100,13 +100,13 @@ def compute_proportional_allocation(df_investor, types_to_exclude):
     allocation = df_investor[df_investor['tipo'] == 'partplanprev'].drop(columns=['valor_calc'])
 
     invstr_filtrd = df_investor[~df_investor['tipo'].isin(types_to_exclude + ['partplanprev'])]
-
-    rows_allocated = invstr_filtrd.index
+    invstr_filtrd.loc[:, 'original_index'] = invstr_filtrd.index
 
     invstr_filtrd.loc[:, ['new_tipo']] = invstr_filtrd['tipo']
 
+    columns_filtrd = ['codcart', 'nome', 'valor_calc', 'new_tipo', 'original_index']
     allocation_value = allocation.merge(
-        invstr_filtrd[['codcart', 'nome', 'valor_calc', 'new_tipo']].dropna(subset=['valor_calc']),
+        invstr_filtrd[columns_filtrd].dropna(subset=['valor_calc']),
         on=['codcart', 'nome'],
         how='inner'
     )
@@ -123,7 +123,7 @@ def compute_proportional_allocation(df_investor, types_to_exclude):
     allocation_value['flag_rateio'] = 0
     allocation_value.drop('new_tipo', axis=1, inplace=True)
 
-    return allocation_value, rows_allocated
+    return allocation_value
 
 
 def harmonize_values(dtfr, harmonization_rules):
@@ -247,10 +247,9 @@ def main():
     keys_not_allocated = [key for key, value in header_daily_values.items() if value.get('serie', False)]
     types_series = [key for key, value in header_daily_values.items() if value.get('serie', True)]
 
-    proprtnl_allocation, rows_allocated = compute_proportional_allocation(portfolios,
-                                                                          keys_not_allocated)
+    proprtnl_allocation = compute_proportional_allocation(portfolios, keys_not_allocated)
 
-    portfolios['flag_rateio'] = portfolios.index.isin(rows_allocated).astype(int)
+    portfolios['flag_rateio'] = portfolios.index.isin(proprtnl_allocation['original_index'].unique()).astype(int)
 
     portfolios = pd.concat([
         portfolios,
