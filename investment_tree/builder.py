b50640aa3cac350be83887225b76b5fd0b6e06e3
Kaluana Software <kaluana.software@outlook.com>
2026-05-06 20:52:25 -0300

    Adiciona 'coupom', 'indexador', 'caracteristica'.

--- investment_tree/builder.py
+++ investment_tree/builder.py
@@ -159,7 +159,7 @@ def build_tree(funds, portfolios):
     cols_common = ['dtposicao', 'cnpjfundo', 'nome', 'equity_stake', 'valor_calc',
                   'isin', 'NEW_TIPO', 'fNUMERACA.DESCRICAO', 'fNUMERACA.TIPO_ATIVO',
                   'fEMISSOR.NOME_EMISSOR', 'NEW_NOME_ATIVO', 'NEW_GESTOR',
-                  'NEW_GESTOR_WORD_CLOUD', 'rentab', 'caracteristica']
+                  'NEW_GESTOR_WORD_CLOUD', 'rentab', 'caracteristica', 'coupom', 'indexador']
 
     funds = funds[funds['valor_serie'] == 0][['cnpj'] + cols_common].copy()
 
